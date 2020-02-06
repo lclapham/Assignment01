fdab2f8bbcd396b1f8821fc43b1cc6e4a133064c
Lamar Clapham <lamar.clapham@gmail.com>
2020-02-06 15:31:26 -0800

    Adding 1-13 steps

---
+++ js/script.js
@@ -0,0 +1,84 @@
+// STEP 1
+
+var someMonth;
+function theMonth ();
+var summerMonth;
+var myLibaryFunction;
+
+// STEP 2
+
+let item1 = 3.9;
+let item2 = "Hello World";
+let item3 = true;
+let item4 = null;
+
+// STEP 3
+
+let exampleOne = 3 * (4 / 5) + 6;
+let exampleTwo = Math.PI * radius * radius;
+
+
+// STEP 4
+
+let firstName, lastName, address, city, zipCode, yourAge, referralSource, mayWeContactYou;
+
+// STEP 5
+
+firstName = "John";
+lastName = "Doe";
+let address = "11884 Round Street";
+let city = "San Diego", zipCode = 920128;
+yourAge = 35;
+let referralSource = "website";
+let mayWeContactYou = true;
+
+// STEP 6
+
+let aVariable = 10;
+window.console.log("This is a string coerced with a number that is " +aVariable)
+
+// STEP 7
+
+let varOne, varTwo;
+varOne = true;
+window.console.log("Adding a boolean to a string " +varOne)
+
+varTwo = 100;
+window.console.log(varOne + varTwo)  //will be 101
+
+// STEP 8
+// No Needs to be escaped at the I'm
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+// window.console.log(someString);
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+
+// STEP 9
+
+let emptyBox = null;
+window.console.log("This is an example of a " +emptyBox+ " varible.");
+
+// STEP 10
+
+// Using STEP 5 & 9 variables
+ley myObject = [1,2,3,4,5];
+
+window.console.log(typeof(firstName)); // String
+window.console.log(typeof(yourAge)); // Number
+window.console.log(typeof(mayWeContactYou));// Boolean
+window.console.log(typeof(myObject));// Object
+window.console.log(typeof(emptyBox)); // Null
+
+// STEP 11
+
+let partOneString = "Hello";
+let partTwoString = "welcome to the JavaScript class!"
+window.alert(partOneString + " Lamar Clapham, " +partTwoString);
+
+// STEP 12
+
+let name = "Lamar Clapham";
+window.alert(partOneString + " "+ name + ", "+ partTwoString);
+
+// STEP 13
