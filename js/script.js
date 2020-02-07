e2df6eaf43eb6f5c7a25194dafe23a1c1b8932fd
Lamar Clapham <lamar.clapham@gmail.com>
2020-02-06 20:05:29 -0800

    Completed 13-25

--- js/script.js
+++ js/script.js
@@ -1,84 +1,158 @@
 // STEP 1
-
-var someMonth;
-function theMonth ();
-var summerMonth;
-var myLibaryFunction;
+// var someMonth;
+// function theMonth ();
+// var summerMonth;
+// var myLibaryFunction;
 
 // STEP 2
-
-let item1 = 3.9;
-let item2 = "Hello World";
-let item3 = true;
-let item4 = null;
+// let item1 = 3.9;
+// let item2 = "Hello World";
+// let item3 = true;
+// let item4 = null;
 
 // STEP 3
-
-let exampleOne = 3 * (4 / 5) + 6;
-let exampleTwo = Math.PI * radius * radius;
+// let exampleOne = 3 * (4 / 5) + 6;
+//These won't actually work because the variables in the assignment is not declared--shown for examples only.
+// let exampleTwo = Math.PI * radius * radius;
 
 
 // STEP 4
-
-let firstName, lastName, address, city, zipCode, yourAge, referralSource, mayWeContactYou;
+// let firstName, lastName, address, city, zipCode, yourAge, referralSource, mayWeContactYou;
 
 // STEP 5
-
-firstName = "John";
-lastName = "Doe";
-let address = "11884 Round Street";
-let city = "San Diego", zipCode = 920128;
-yourAge = 35;
-let referralSource = "website";
-let mayWeContactYou = true;
+// firstName = "John";
+// lastName = "Doe";
+// let address = "11884 Round Street";
+// let city = "San Diego", zipCode = 920128;
+// yourAge = 35;
+// let referralSource = "website";
+// let mayWeContactYou = true;
 
 // STEP 6
-
-let aVariable = 10;
-window.console.log("This is a string coerced with a number that is " +aVariable)
+// let aVariable = 10;
+// window.console.log("This is a string coerced with a number that is " +aVariable)
 
 // STEP 7
+// let varOne, varTwo;
+// varOne = true;
+// window.console.log("Adding a boolean to a string " +varOne)
 
-let varOne, varTwo;
-varOne = true;
-window.console.log("Adding a boolean to a string " +varOne)
-
-varTwo = 100;
-window.console.log(varOne + varTwo)  //will be 101
+// varTwo = 100;
+// window.console.log(varOne + varTwo)  //will be 101
 
 // STEP 8
 // No Needs to be escaped at the I'm
 // var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
 // window.console.log(someString);
 
-var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
-window.console.log(someString);
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
 
 // STEP 9
-
-let emptyBox = null;
-window.console.log("This is an example of a " +emptyBox+ " varible.");
+// let emptyBox = null;
+// window.console.log("This is an example of a " +emptyBox+ " varible.");
 
 // STEP 10
+// let myObject = [1,2,3,4,5];
+// let firstName = "John";
+// let emptyBox = null;
+// let address = "11884 Round Street";
+// let yourAge = 35;
+// let mayWeContactYou = true;
 
-// Using STEP 5 & 9 variables
-ley myObject = [1,2,3,4,5];
-
-window.console.log(typeof(firstName)); // String
-window.console.log(typeof(yourAge)); // Number
-window.console.log(typeof(mayWeContactYou));// Boolean
-window.console.log(typeof(myObject));// Object
-window.console.log(typeof(emptyBox)); // Null
+// window.console.log(typeof(firstName)); // String
+// window.console.log(typeof(yourAge)); // Number
+// window.console.log(typeof(mayWeContactYou));// Boolean
+// window.console.log(typeof(myObject));// Object
+// window.console.log(typeof(emptyBox)); // Null
 
 // STEP 11
-
-let partOneString = "Hello";
-let partTwoString = "welcome to the JavaScript class!"
-window.alert(partOneString + " Lamar Clapham, " +partTwoString);
+// let partOneString = "Hello";
+// let partTwoString = "welcome to the JavaScript class!"
+// window.alert(partOneString + " Lamar Clapham, " +partTwoString);
 
 // STEP 12
-
-let name = "Lamar Clapham";
-window.alert(partOneString + " "+ name + ", "+ partTwoString);
+// let partOneString = "Hello";
+// let partTwoString = "welcome to the JavaScript class!"
+// let name = "Lamar Clapham";
+// window.alert(partOneString + " "+ name + ", "+ partTwoString);
 
 // STEP 13
+// let partOneString = "Hello";
+// let partThreeString = ", welcome to the ";
+// let partFourString = " class!"
+// let course = "JavaScript";
+// let name = "Lamar Clapham";
+// window.alert(partOneString + " "+ name + partThreeString + course + partFourString);
+
+// STEP 14
+// let strHello = "Hello ";
+// let name = "Lamar Clapham";
+// let course = "JavaScript";
+// let strWelcome = "Welcome to the ";
+// let strClass = " class!";
+// window.alert(strHello + name +".\n"  + strWelcome + " " + course + strClass);
+
+// STEP 15
+// let strHello = "Hello ";
+// let name = prompt("Please type in your name.");
+// let course = "JavaScript";
+// let strWelcome = "Welcome to the ";
+// let strClass = " class!";
+// window.alert(strHello + name +".\n"  + strWelcome + " " + course + strClass);
+
+// STEP 16
+// let strHello = "Hello ";
+// let name = prompt("Please type in your name.");
+// let course = prompt("What class are you taking?", "Javascript")
+// let strWelcome = "Welcome to the ";
+// let strClass = " class!";
+// window.alert(strHello + name +".\n"  + strWelcome + " " + course + strClass);
+
+// STEP 17
+// let x, y;
+// x = 10;
+// y = 20;
+// window.console.log("The sum of x and y is " + (x+y));
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// window.console.log("The value of x is " +x);
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// window.console.log("The value of x is " +x);
+
+// STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// window.console.log(x)
+
+//STEP 21
+// let x = 10;
+// let y = 20;
+// let z = 20;
+// window.console.log(x === 10 && y === z);
+
+
+//STEP 22
+// let x = 10;
+// let y = 20;
+// let z = 20;
+// window.console.log(x !== 10 || y < z);
+
+//STEP 23
+// let widget = new Object();
+// window.console.log(typeof(widget));
+
+//STEP 24
+// let widget = new Object();
+// window.console.log(widget instanceof Object);
+
+//STEP 25
+// let widget = new Object();
+// window.console.log(!widget instanceof Object);
+
+
